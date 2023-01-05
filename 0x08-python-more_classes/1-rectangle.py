0e0ba3764da80f349e0dddeef60ae95e2f15b167
Jeff-20 <maribetshepomakwela@gmail.com>
2023-01-05 20:47:37 +0200

    real def of rect

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,32 @@
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if value != round(value):
+            if value < 0:
+                raise ValueError
+                print('width must be >= 0')
+            raise TypeError
+            print('width must be an integer')
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if value != round(value):
+            if value < 0:
+                raise ValueError
+                print('height must be >= 0')
+            raise TypeError
+            print('height must be an integer')
+        self.__height = value

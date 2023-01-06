3520d660b75c30d1c62f106e267d13c50d3e9532
Jeff-20 <maribetshepomakwela@gmail.com>
2023-01-06 13:43:28 +0200

    real def of rect

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if value != round(value):
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -28,7 +28,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if value != round(value):
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')

82468acd1bc16ebbe65a370fc76d41fbc6c2390b
Jeff-20 <maribetshepomakwela@gmail.com>
2023-06-14 01:47:48 +0200

    0.Read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,6 +7,8 @@
 
 
 def read_file(filename=""):
-
+    """Reads 'filename' and prints its content to
+        STDOUT
+    """
     with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")

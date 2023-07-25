f6f763b2a74698e36dff86bdf3f458538fa80c01
Jeff-20 <maribetshepomakwela@gmail.com>
2023-07-25 02:17:02 +0200

    4.first table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- creates table 'first_table' in the current database
+-- in my MySQL server. The database name is passed as
+-- an argument of the 'mysql' command
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));

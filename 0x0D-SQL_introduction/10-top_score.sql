9430ae9c7fa434067d220d8c527119bd13d4b245
Jeff-20 <maribetshepomakwela@gmail.com>
2023-07-26 01:20:45 +0200

    10 list by best

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,7 @@
+-- Lists all the records of 'name' and 'score'
+-- of the table 'second_table' in this order,
+-- of the database 'htbn_0c_0' in MySQL server.
+-- The records are ordered by 'score' in descending
+-- order.
+SELECT score, name FROM second_table
+ORDER BY score DESC;

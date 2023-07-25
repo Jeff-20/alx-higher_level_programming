d52e5e254e7acd0cd78694ccbb707e570dee5d9d
Jeff-20 <maribetshepomakwela@gmail.com>
2023-07-26 00:40:36 +0200

    fixing task 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -2,4 +2,4 @@
 -- in the table first_table of the database
 -- htbn_0c_0 in MySQL server. The database name
 -- is passed as the argument for the mysql command
-SELECT COUNT(id) FROM first_table;
+SELECT COUNT(id) FROM first_table WHERE id=89;

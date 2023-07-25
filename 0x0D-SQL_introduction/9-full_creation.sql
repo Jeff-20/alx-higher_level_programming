4e9404fea0923b0e6dc15bb5540fda20a834080d
Jeff-20 <maribetshepomakwela@gmail.com>
2023-07-26 00:56:02 +0200

    adding task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,7 @@
 -- creates a table 'second_table' in the database htbn_0c_0
 -- in MySQL server and multiple rows
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score) VALUE(1, 'John',
-           'Bob', 10)
+INSERT INTO second_table(id, name, score) VALUE(1, 'John', 10);
+INSERT INTO second_table(id, name, score) VALUE(2, 'Alex', 3);
+INSERT INTO second_table(id, name, score) VALUE(3, 'Bob', 14);
+INSERT INTO second_table(id, name, score) VALUE(4, 'George', 8);

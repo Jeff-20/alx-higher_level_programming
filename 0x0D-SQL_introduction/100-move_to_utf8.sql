3436deb3c5fef989e2ab0f5a2b76624400077c49
Jeff-20 <maribetshepomakwela@gmail.com>
2023-07-26 14:34:37 +0200

    17 fixing

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,7 +8,7 @@ CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- first_table to UTF8
--- USE hbtn_0c_0;
--- ALTER TABLE first_table
--- CONVERT TO CHARACTER SET utf8mb4
--- COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;

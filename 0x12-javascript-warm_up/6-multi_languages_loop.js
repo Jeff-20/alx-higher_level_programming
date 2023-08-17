edff12d847cf23e75f126d99e7f93212233fdde1
Jeff-20 <maribetshepomakwela@gmail.com>
2023-08-17 02:39:01 +0200

    6.loop to lang

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Prints 3 lines by using an array of string and a loop
+
+const stringArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < stringArray.length; i++) {
+  console.log(stringArray[i]);
+}

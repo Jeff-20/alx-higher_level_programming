52ccc6405aa255bb63d4d6ca11f4acbb6a426eaf
Jeff-20 <maribetshepomakwela@gmail.com>
2023-08-17 04:07:04 +0200

    9.add

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Prints the sum of 2 integers
+
+const firstArg = parseInt(process.argv[2]);
+const secondArg = parseInt(process.argv[3]);
+
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  }
+  return a + b;
+}
+console.log(add(firstArg, secondArg));

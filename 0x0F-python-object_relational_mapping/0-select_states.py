ae7b20157c887b93bc4a8142a4c0a6bbaefc0c6f
Jeff-20 <maribetshepomakwela@gmail.com>
2023-08-23 13:22:31 +0200

    removing host and port

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,7 +8,7 @@ import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], host='localhost', port=3306)
+                         db=sys.argv[3])
 
     c = db.cursor()
     c.execute("SELECT id, name FROM states ORDER BY id")

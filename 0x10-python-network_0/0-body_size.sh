576dd34f805e64cef8ac1d7dfccd22352a13328a
Jeff-20 <maribetshepomakwela@gmail.com>
2023-09-03 10:33:24 +0200

    0.curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script takes a URL, sends a request to it,
-# and displays the sze of the body of the response
-
+# This script takes a URL, sends a request to it, and displays the size of the response body
 curl -sI "$1" | grep Content-Length | cut -d " " -f 2

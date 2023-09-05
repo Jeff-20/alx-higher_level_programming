e611f1bf5da32e68064978c2179da92d1a32cca4
Jeff-20 <maribetshepomakwela@gmail.com>
2023-09-05 22:38:42 +0200

    fixing 6 again

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,6 +11,8 @@ from sqlalchemy import (create_engine)
 """
 
 if __name__ == "__main__":
+    """I am just testing documentation here
+    """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
